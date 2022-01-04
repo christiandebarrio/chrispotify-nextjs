2e4c7f5cb15bc24c62db7d5111e7693420e7bf69
Christian de Barrio <christian.debarrio@cabify.com>
2022-01-04 21:17:10 +0100

    fetching user playlist

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,15 +8,28 @@ import {
   LogoutIcon,
 } from "@heroicons/react/outline";
 import { signOut, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useSpotify } from "../hooks/useSpotify";
 
 export const Sidebar = () => {
   const { data: session, status } = useSession();
+  const spotifyApi = useSpotify();
+  const [playlists, setPlaylists] = useState<
+    SpotifyApi.PlaylistObjectSimplified[]
+  >([]);
 
-  console.log(session, status);
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi.getUserPlaylists().then((data) => {
+        setPlaylists(data.body.items);
+      });
+    }
+  }, [session, spotifyApi]);
+
   const handleLogout = () => signOut();
 
   return (
-    <aside className="text-gray-500 p-5 text-sm border-r border-gray-900">
+    <aside className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll h-screen">
       <div className="space-y-4">
         <button
           onClick={handleLogout}
@@ -55,16 +68,11 @@ export const Sidebar = () => {
 
         <hr className="border-t-[0.1px] border-gray-900" />
 
-        {/* Playlist */}
-        <p className="cursor-pointer hover:text-white">Playlist name</p>
-        <p className="cursor-pointer hover:text-white">Playlist name</p>
-        <p className="cursor-pointer hover:text-white">Playlist name</p>
-        <p className="cursor-pointer hover:text-white">Playlist name</p>
-        <p className="cursor-pointer hover:text-white">Playlist name</p>
-        <p className="cursor-pointer hover:text-white">Playlist name</p>
-        <p className="cursor-pointer hover:text-white">Playlist name</p>
-        <p className="cursor-pointer hover:text-white">Playlist name</p>
-        <p className="cursor-pointer hover:text-white">Playlist name</p>
+        {playlists.map((playlist) => (
+          <p className="cursor-pointer hover:text-white" key={playlist.id}>
+            {playlist.name}
+          </p>
+        ))}
       </div>
     </aside>
   );
